401339415f088960a4e9e2a12431c1b4669d55be
dexter <dexter.haan@gmail.com>
2020-02-23 01:18:14 +0900

    swagger

--- src/signs/dto/sign.dto.ts
+++ src/signs/dto/sign.dto.ts
@@ -1,11 +1,14 @@
-import { IsInt, IsUrl, IsString } from 'class-validator';
+import { IsUrl, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateSignDto {
+    @ApiProperty()
     @IsString() // TODO: @IsUrl()
     url: string;
 }
 
 export class UpdateSignDto {
+    @ApiProperty()
     @IsString()  // TODO: @IsUrl()
     url: string;
 }
