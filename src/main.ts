66985f02ac921a8c483f80fd4ff56f03dc746fc9
dexter <dexter.haan@gmail.com>
2020-02-23 01:18:14 +0900

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger setting
+  const options = new DocumentBuilder()
+    .setTitle('Modusign API')
+    .setDescription('The Modusign API description')
+    .setVersion('0.1')
+    // .addTag('modusign')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
--- src/teams/dto/team.dto.ts
+++ src/teams/dto/team.dto.ts
@@ -1,6 +1,8 @@
 import { IsInt } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTeamDto {
+    @ApiProperty()
     @IsInt()
     signId: number;
 }
