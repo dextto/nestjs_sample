520409acd27d3f04f2334246dce0922d71ed417a
dexter <dexter.haan@gmail.com>
2020-02-23 21:54:41 +0900

    user controller test - create

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Payload } from '@nestjs/microservices';
 import { CreateUserDto } from './dto/create-user.dto';
-import { Roles } from 'src/roles.decorator';
+import { Roles } from '../roles.decorator';
 
 
 @Controller('user')
