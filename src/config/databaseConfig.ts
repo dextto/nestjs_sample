2a24ebdb08fb64ea256c30d015de5fec55e767fd
dexter <dexter.haan@gmail.com>
2021-01-23 17:41:31 +0900

    chore: 로컬 디비 비번 변경

--- src/config/databaseConfig.ts
+++ src/config/databaseConfig.ts
@@ -4,7 +4,7 @@ export default () => ({
     host: process.env.DATABASE_HOST || 'localhost',
     port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
     username: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD || 'root',
+    password: process.env.DATABASE_PASSWORD || 'test',
     dbName: process.env.DATABASE_DB_NAME || 'test',
   }
 });
